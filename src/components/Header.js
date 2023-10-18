71d6a274bfb27dd9106a4504dc48afb600ed5b55
takoko55 <ai625292@gmail.com>
2023-10-18 17:20:17 +0900

    create header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="todo-header">
+        <p>Todoリスト</p>
+    </header>
+  );
+}
+
+export default Header;
