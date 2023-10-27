98a710fc8599f4ddd275b759d41536fdf52d9678
takoko55 <ai625292@gmail.com>
2023-10-27 15:31:03 +0900

    modify new task button

--- src/components/NewTaskButton.js
+++ src/components/NewTaskButton.js
@@ -4,7 +4,7 @@ import "./NewTaskButton.css";
 function NewTaskButton() {
     return(
         <div class="newtaskbutton-container">
-            <button class="newtaskbutton">＋ 新規作成</button>
+            <button class="newtaskbutton" title="タスクの新規作成を行います">＋ 新規作成</button>
         </div>
     );
 }
